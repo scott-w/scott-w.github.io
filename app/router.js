44f4d4cc96a98149e0ab4bd466ceb2ad7273a0d5
Scott Walton <s@cott.me.uk>
2016-01-16 08:40:37 +0000

    Update Comments

--- app/router.js
+++ app/router.js
@@ -5,40 +5,42 @@ var Interests = require('./models/interests');
 
 
 /** The main logic that handles movement across screens. The application
-* initializer should delegate most of the work here as soon as possible, and let
-* the individual views manage themselves.
-*/
+ * initializer should delegate most of the work here as soon as possible, and let
+ * the individual views manage themselves.
+ */
 var Controller = Backbone.Marionette.Controller.extend({
 
   /** We need the app and collection in options here so we can effectively
-  * manage the rendering of the whole page. Using this method means we don't
-  * need to manage the Backbone history ourselves because the browser is going
-  * to do it all for us.
-  */
-  initialize: function(options) {
+   * manage the rendering of the whole page. Using this method means we don't
+   * need to manage the Backbone history ourselves because the browser is going
+   * to do it all for us.
+   */
+  initialize: function (options) {
     this.options = {
       app: options.app,
-      interests: new Interests({}, {parse: true})
+      interests: new Interests({}, {
+        parse: true
+      })
     };
     /* Run the fetch in the initalize, so if the user hits the homepage, the
-    * data is pre-cached and ready to display immediately.
-    */
+     * data is pre-cached and ready to display immediately.
+     */
     this.options.interests.fetch();
     this.collection = options.collection;
   },
 
   /** Displays the homepage.
-  */
-  home: function() {
+   */
+  home: function () {
     this.options.app.main.show(new IndexView())
     this._setActive('');
   },
 
   /** Displays the list of interests. The interest model is pre-fetched from
-  * the initialize method, so most of the time, there will be little to no
-  * delay in rendering.
-  */
-  interests: function() {
+   * the initialize method, so most of the time, there will be little to no
+   * delay in rendering.
+   */
+  interests: function () {
     var view = new InterestView({
       model: this.options.interests
     });
@@ -47,12 +49,16 @@ var Controller = Backbone.Marionette.Controller.extend({
   },
 
   /** Set the active tab in the navigation list at the top of the screen.
-  * This works by updating the shared Backbone model, which will trigger the
-  * NavigationView to re-render its own list.
-  */
-  _setActive: function(href) {
-    var newActive = this.collection.findWhere({href: href});
-    var oldActive = this.collection.findWhere({active: true});
+   * This works by updating the shared Backbone model, which will trigger the
+   * NavigationView to re-render its own list.
+   */
+  _setActive: function (href) {
+    var newActive = this.collection.findWhere({
+      href: href
+    });
+    var oldActive = this.collection.findWhere({
+      active: true
+    });
 
     if (oldActive.get('href') != newActive.get('href')) {
       newActive.set('active', true);
@@ -64,10 +70,10 @@ var Controller = Backbone.Marionette.Controller.extend({
 
 var Router = Backbone.Marionette.AppRouter.extend({
   /** We need this so we can feed the app and collection options through to the
-  * controller. We could also expose the controller directly to the application
-  * and manage it there if we wished.
-  */
-  initialize: function(options) {
+   * controller. We could also expose the controller directly to the application
+   * and manage it there if we wished.
+   */
+  initialize: function (options) {
     this.controller = new Controller(options);
   },
 
--- app/views/interests.js
+++ app/views/interests.js
@@ -3,30 +3,30 @@ var markdown = require('markdown').markdown;
 
 
 /** We render the model if necessary. We use a LayoutView here out of habit.
-* Making everything a LayoutView gives us access to the region hash if we want
-* to use it later.
-*/
+ * Making everything a LayoutView gives us access to the region hash if we want
+ * to use it later.
+ */
 var InterestsView = Backbone.Marionette.LayoutView.extend({
   template: require('../templates/interests.html'),
 
   /** Pre-save the jQuery selector.
-  */
+   */
   ui: {
     interests: '.interest-hook'
   },
 
   /** Whenever the fetch completes, re-render this view.
-  */
+   */
   modelEvents: {
     sync: 'render'
   },
 
   templateHelpers: {
     /** Lets us access a markdown() function in the template itself.
-    * This is the meat of the view, as the model attribute is just a markdown
-    * string.
-    */
-    markdown: function(attribute) {
+     * This is the meat of the view, as the model attribute is just a markdown
+     * string.
+     */
+    markdown: function (attribute) {
       if (attribute) {
         return markdown.toHTML(attribute);
       }
@@ -35,9 +35,9 @@ var InterestsView = Backbone.Marionette.LayoutView.extend({
   },
 
   /** Called after render() completes. We use this to create the HTML class
-  * we need to get the list to render cleanly after markdown renders it.
-  */
-  onRender: function() {
+   * we need to get the list to render cleanly after markdown renders it.
+   */
+  onRender: function () {
     this.ui.interests.children('ul').addClass('list-unstyled');
   }
 });
